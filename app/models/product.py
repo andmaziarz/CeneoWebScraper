14dbb9a31460c6e8248f565ffaa8cc772b98598c
andmaziarz <a.maziarz9@outlook.com>
2022-06-10 01:29:09 +0200

    2022-06-09 homework

--- app/models/product.py
+++ app/models/product.py
@@ -1,4 +1,7 @@
 class Product():
-    def __init__(self):
+    def __init__(self, name, price, description, opinions):
+        self.name=name
+        self.price=price
+        self.description=description
+        self.opinions=opinions
         pass
-    
