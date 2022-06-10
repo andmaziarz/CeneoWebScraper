44e649cb8e09f0f760f11d9fcd76f827feefd748
andmaziarz <a.maziarz9@outlook.com>
2022-06-10 13:00:44 +0200

    2022-06-10 AM

--- app/models/opinion.py
+++ app/models/opinion.py
@@ -1,5 +1,8 @@
+from app import parameters
+from app.utils import get_item
+
 class Opinion():
-    def __init__(self, opinion_id, author, recommendation, stars, content, pros, cons, useful, useless, published, purchased):
+    def __init__(self, opinion_id="", author="", recommendation=None, stars=0, content="", pros=[], cons=[], useful=0, useless=0, published_date=None, purchased_date=None):
         self.opinion_id=opinion_id
         self.author=author
         self.recommendation=recommendation
@@ -9,6 +12,22 @@ class Opinion():
         self.cons=cons
         self.useful=useful
         self.useless=useless
-        self.published=published
-        self.purchased=purchased
+        self.published_date=published_date
+        self.purchased_date=purchased_date
+        return self
+    
+    def extract_opinion(self, opinion):
+        for key, value in selectors.items():
+            setattr(self, key, get_item(opinion, *value))
+            self.opinion_id = opinion["data-entry-id"]
+        return self
+
+    def __str__(self) -> str:
         pass
+
+    def __repr__(self) -> str:
+        pass
+
+    def to_dict(self) -> dict:
+        pass
+        
--- app/models/product.py
+++ app/models/product.py
@@ -1,7 +1,116 @@
+import json
+import requests
+import os
+from bs4 import BeautifulSoup
+from app.utils import get_item
+from app.models.opinion import Opinion
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
 class Product():
-    def __init__(self, name, price, description, opinions):
-        self.name=name
+    def __init__(self, product_id, product_name, price, description, opinions=[], opinions_count=0):
+        self.product_name=product_name
+        self.product_id=product_id
         self.price=price
         self.description=description
         self.opinions=opinions
+        self.opinions_count=opinions_count
+        return self
+
+    def extract_name(self):
+        url = f"https://www.ceneo.pl/{self.product_id}#tab=reviews"
+        response = requests.get(url)
+        page = BeautifulSoup(response.text, "html.parser")
+        self.product_name = get_item(page, "h1.product-top__product-info__name")
+
+        return self
+    
+    def extract_opinions(self):
+        url = f"https://www.ceneo.pl/{self.product_id}#tab=reviews"
+        all_opinions = []
+        while(url):
+            response = requests.get(url)
+            page = BeautifulSoup(response.text, "html.parser")
+            opinions = page.select("div.js_product-review")
+            for opinion in opinions:
+                single_opinion = Opinion().extract_opinion(opinion)
+                self.opinions.append(single_opinion)
+            try:
+                url = "https://www.ceneo.pl"+get_item(page, "a.pagination__next", "href")
+            except TypeError:
+                url = None
+        return self
+
+    def opinions_to_df(self):
+        return pd.read_json(json.dumps([opinion.to_dict() for opinion in self.opinions]))
+
+    def calculate_stats(self):
+
+        self.opinions_to_df()
+        opinions["stars"] = opinions["stars"].map(lambda x: float(x.split("/")[0].replace(",", ".")))
+
+        self.opinions_count = len(opinions)
+        self.pros_count = opinions["pros"].map(bool).sum()
+        self.cons_count = opinions["cons"].map(bool).sum()
+        self.average_score = opinions["stars"].mean().round(2)
+
+        return self
+
+    def draw_charts(self):
+        self.opinions_to_df()
+
+        if not os.path.exists("app/plots"):
+            os.mkdir("app/plots")
+
+        recommendation = opinions["recommendation"].value_counts(dropna=False).sort_index().reindex(["Nie polecam", "Polecam", None], fill_value=0)
+        recommendation.plot.pie(
+            label="",
+            autopct= lambda p: '{:.1f}%'.format(round(p)) if p > 0 else '',
+            colors = ["crimson","forestgreen","lightgrey"],
+            labels=["Nie polecam","Polecam","Nie mam zdania"]
+
+        )
+        plt.title("Rekomendacje")
+        plt.savefig(f"app/static/plots/{self.product_id}_recommendations.png")
+        plt.close()
+
+        stars = opinions["stars"].value_counts().sort_index().reindex(list(np.arange(0,5.5,0.5)), fill_value=0)
+        stars.plot.bar(
+            color = "#fbec5d"
+        )
+
+        plt.title("Oceny produktów")
+        plt.xlabel("Liczba gwiazdek")
+        plt.ylabel("Liczba opinii")
+        plt.grid(True, axis="y")
+        plt.xticks(rotation=0)
+        plt.savefig(f"app/static/plots/{self.product_id}_stars.png")
+        plt.close()
+
+        return self
+    
+    def __str__(self) -> str:
+        #homework, czyli zrzutowanie obiektu do stringa
+        pass
+
+    def __repr__(self) -> str:
+        #homework, konstruktor kopiujący, reprodukcja , wywołanie konstruktora z wartościami bieżącego obiektu
+        pass
+
+    def to_dict(self) -> dict:
+        #homework, zwracanie słownika
+        pass
+
+    def export_opinions(self):
+        if not os.path.exists("app/opinions"):
+            os.mkdir("app/opinions")
+        
+        with open(f"app/opinions/{self.product_id}.json", "w", encoding="UTF-8") as jf:
+            json.dump([opinion.to_dict() for opinion in self.opinions], jf, indent=4, ensure_ascii=False)
+            
+        pass
+
+    def export_product(self):
         pass
